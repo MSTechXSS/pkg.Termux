39396348820a5b05f2d743554826effc4ed1ba44
Mahmoud Safwat <30844339+MSTechXSS@users.noreply.github.com>
2020-12-15 20:38:40 +0200

    Add files via upload

---
+++ mstech.sh
@@ -0,0 +1,56 @@
+printf "\e[1;36mWelcome to my tool (pkg.Termux), you can install the basic commands you need Termux and solve all the problems that you will face when installing any tool"
+printf "\n"
+printf "\e[1;36
+                  ███╗░░░███╗░██████╗
+                  ████╗░████║██╔════╝
+                  ██╔████╔██║╚█████╗░
+                  ██║╚██╔╝██║░╚═══██╗
+                  ██║░╚═╝░██║██████╔╝
+                  ╚═╝░░░░░╚═╝╚═════╝░
+
+           ████████╗███████╗░█████╗░██╗░░██╗
+           ╚══██╔══╝██╔════╝██╔══██╗██║░░██║
+           ░░░██║░░░█████╗░░██║░░╚═╝███████║
+           ░░░██║░░░██╔══╝░░██║░░██╗██╔══██║
+           ░░░██║░░░███████╗╚█████╔╝██║░░██║
+           ░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝"
+printf "\n"
+printf "\e[1;32m"
+pkg install root-repo -y
+termux-setup-storage -y
+pkg update -y
+pkg upgrade -y
+pkg install php -y
+pkg install openssh -y
+pkg install github -y
+pkg install figlet -y
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install perl
+pkg install ruby
+pkg install wget
+git clone https://github.com/MFDGaming/ubuntu-in-termux
+cd ~
+printf "\e[1;36m Termux Install Finished .. you can following more in my Channel and pages (MS TECH) 
+
+My account is on Reddit:
+www.reddit.com/user/MahmoudSafwat
+
+Website : 
+www.everytechs.com
+
+My channel link : 
+www.youtube.com/MSTech-0
+
+Link my Facebook page : 
+www.facebook.com/MSTech.0
+
+My Facebook profile
+FB.com/mahmoud.safwat0
+
+Tool By Mahmoud Safwat
+
+Thank You "
+printf "\n"
