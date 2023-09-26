78ea5e61c07f852b7beb31963814fe0e40c5769f
Mahmoud Safwat <30844339+MSTechXSS@users.noreply.github.com>
2023-09-26 03:50:34 +0300

    Update mstech.sh

--- mstech.sh
+++ mstech.sh
@@ -16,20 +16,23 @@ printf "\e[1;36
            ░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝"
 printf "\n"
 printf "\e[1;32m"
-pkg install root-repo -y
-termux-setup-storage -y
+pkg install root-repo
+termux-setup-storage
 pkg update -y
 pkg upgrade -y
 pkg install php -y
 pkg install openssh -y
-pkg install github -y
+pkg install git -y
+git install github -y
 pkg install figlet -y
 pkg install python -y
 pkg install python2 -y
 pkg install python3 -y
-pkg install perl
-pkg install ruby
-pkg install wget
+pkg install perl -y
+pkg install ruby -y
+pkg install wget -y
+pkg install nano -y
+pkg install curl -y
 git clone https://github.com/MFDGaming/ubuntu-in-termux
 cd ~
 printf "\e[1;36m Termux Install Finished .. you can following more in my Channel and pages (MS TECH) 
